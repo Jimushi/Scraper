00db8fedc339274d7697695cad3a1f760231ba4c
Jimushi <joaocosta-95@hotmail.com>
2021-11-10 13:11:56 +0000

    Co-authored-by: Eduardo Costa <ed_cscosta@hotmail.com>

--- models/Promotion.js
+++ models/Promotion.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const PromotionSchema = new mongoose.Schema({
   promotion: String,
   category: String,
-  link: String
+  link: String,
+  timestamps: true
 })
 
 module.exports = mongoose.model('Promotion', PromotionSchema)
